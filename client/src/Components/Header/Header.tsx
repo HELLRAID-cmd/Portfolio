923272d1b61bc3bbb3886ef4b5351270f68acee6
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-21 14:28:38 +0500

    Добавил header с темой

--- client/src/Components/Header/Header.tsx
+++ client/src/Components/Header/Header.tsx
@@ -1,9 +1,58 @@
-import "./Header.scss"
+import { Button } from "antd";
+import "./Header.scss";
+import { SunFilled, MoonFilled } from "@ant-design/icons";
+import { useState } from "react";
 
 export const Header = () => {
+  const [dark, setDark] = useState(false);
+
+  const toggleTheme = () => {
+    setDark(!dark);
+  }
+  
   return (
     <header className="header">
-      <h1>My Portfolio</h1>
+      <div className="container">
+        <div className="header__wrapper">
+          <a href="index.html" className="header__icon-link">
+          <svg
+            className="header__icon"
+            width="64"
+            height="64"
+            aria-hidden="true"
+          >
+            <use href="/public/svg/sprite.svg#icon-user"></use>
+          </svg>
+          </a>
+          <ul className="header__menu">
+            <li className="header__menu-item">
+              <a className="header__menu-item-link" href="#!">
+                About
+              </a>
+            </li>
+            <li className="header__menu-item">
+              <a className="header__menu-item-link" href="#!">
+                Skills
+              </a>
+            </li>
+            <li className="header__menu-item">
+              <a className="header__menu-item-link" href="#!">
+                Work Experience
+              </a>
+            </li>
+            <li className="header__menu-item">
+              <a className="header__menu-item-link" href="#!">
+                Contacts
+              </a>
+            </li>
+          </ul>
+          <div className="header__theme">
+            <Button className={`header__theme-button ${dark ? "active" : ""}`} onClick={toggleTheme}>
+              {dark ? <MoonFilled /> : <SunFilled />}
+            </Button>
+          </div>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
