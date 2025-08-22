e7ad83d3817971cfc05f95f72924d6cf6b48a880
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-22 13:52:17 +0500

    Добавил hero

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
 import { Header } from './Components/Header/Header'
-import './Reset.css'
+import './Reset.scss'
 import "./styles/container.scss"
 import "./index.scss"
 import { Layout } from 'antd';
+import { Hero } from './Components/Hero/Hero';
 
 function App() {
   return (
-    <Layout>
+    <Layout style={{backgroundColor: 'unset'}}>
       <Header />
-      <Layout>
-      </Layout>
+      <main>
+        <Hero />
+      </main>
     </Layout>
   )
 }
