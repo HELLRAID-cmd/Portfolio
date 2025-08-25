ca67679241da777c3d912336a09a6b80f159c69f
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-25 13:05:52 +0500

    Поменял местами импорты

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,19 @@
-import { Header } from './Components/Header/Header'
-import './styles/Reset.scss'
-import "./styles/container.scss"
-import "./styles/index.scss"
-import { Layout } from 'antd';
-import { Hero } from './Components/Hero/Hero';
+import { Header } from "./Components/Header/Header";
+import "./styles/Reset.scss";
+import "./styles/container.scss";
+import "./styles/index.scss";
+import { Layout } from "antd";
+import { Hero } from "./Components/Hero/Hero";
 
 function App() {
   return (
-    <Layout style={{backgroundColor: 'unset'}}>
+    <Layout style={{ backgroundColor: "unset" }}>
       <Header />
       <main>
         <Hero />
       </main>
     </Layout>
-  )
+  );
 }
 
-export default App
+export default App;
