8c6b1f24c34c2ec906e1bff75d77a3db3ca9d240
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-25 13:38:29 +0500

    Обернул в hero

--- client/src/Components/Hero/Hero.tsx
+++ client/src/Components/Hero/Hero.tsx
@@ -6,22 +6,27 @@ export const Hero = () => {
   return (
     <>
       <section className="hero-sect">
-        <div className="container">
-          <div className="hero__wrapper">
-            <Typography.Paragraph className="hero__text">
-              <Typography.Paragraph className="hero__text-name">
-                Hello! My name is <br /> Emil Gaynulin
+        <div className="hero">
+          <div className="container">
+            <div className="hero__wrapper">
+              <Typography.Paragraph className="hero__text">
+                <Typography.Paragraph className="hero__text-wrapper">
+                  <Typography.Paragraph className="hero__text-wrapper-name">
+                    Hello! My name is <br /> Emil Gaynulin
+                  </Typography.Paragraph>
+                  <Typography.Text className="hero__text-wrapper-desc">
+                    This is what I can do.
+                  </Typography.Text>
+                </Typography.Paragraph>
+                <Button className="hero__text-button">Check!</Button>
               </Typography.Paragraph>
-              <Typography.Text className="hero__text-desc">
-                This is what I can do
-              </Typography.Text>
-            </Typography.Paragraph>
-            <Button className="hero__wrapper-button" aria-label="Scroll down">
-              <ArrowDownOutlined
-                className="hero__wrapper-button-arrow"
-                height={40}
-              />
-            </Button>
+              <Button className="hero__wrapper-button" aria-label="Scroll down">
+                <ArrowDownOutlined
+                  className="hero__wrapper-button-arrow"
+                  height={40}
+                />
+              </Button>
+            </div>
           </div>
         </div>
       </section>
