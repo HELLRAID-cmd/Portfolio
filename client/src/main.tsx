363883382b00bf172eb3b0de61be6e014886c8f2
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-25 13:05:10 +0500

    Обернул в theme

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { Theme } from './Components/Theme/Theme.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Theme>
+      <App />
+    </Theme>
   </StrictMode>,
 )
