106d9761b773c27eb7581066ae5e5027973dcd6a
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-25 18:33:13 +0500

    Добавил about

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import "./styles/index.scss";
 import { Header } from "./Components/Header/Header";
 import { Layout } from "antd";
 import { Hero } from "./Components/Hero/Hero";
+import { About } from "./Components/About/About";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <About />
       </main>
     </Layout>
   );
