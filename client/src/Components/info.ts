ec0a755d6741fc216cc641e0cbf2667b36f33079
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-26 21:13:43 +0500

    Добавил массив скиллов

---
+++ client/src/Components/info.ts
@@ -0,0 +1,21 @@
+export const skills = [
+  [
+    { id: 1, desc: "I know HTML, JSX, TSX." },
+    { id: 2, desc: "CSS(CSS3), SASS ,Animations" },
+    { id: 3, desc: "JavaScript, TypeScript" },
+    { id: 4, desc: "Adaptive and cross-browser layout" },
+    { id: 5, desc: "BEM modification" },
+    { id: 6, desc: "I layout websites using Figma layouts." },
+    { id: 7, desc: "Experience creating design in figma" },
+    { id: 8, desc: "React" },
+    { id: 9, desc: "Git (GitHub, GitLab)" },
+  ],
+  [
+    { id: 3, desc: "JavaScript" },
+    { id: 4, desc: "TypeScript" },
+  ],
+  [
+    { id: 5, desc: "React" },
+    { id: 6, desc: "SCSS" },
+  ],
+];
