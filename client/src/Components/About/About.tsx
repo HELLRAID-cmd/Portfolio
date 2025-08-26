144e830e06df1502fbd71cf3818f5aeae8620ffd
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-26 10:49:06 +0500

    добавил about

---
+++ client/src/Components/About/About.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "antd";
+
+export const About = () => {
+  return (
+    <>
+      <section className="about-sect">
+        <div className="about">
+          <div className="container">
+            <div className="about__wrapper">
+              asd
+              <Typography.Title level={3}>Professional skills</Typography.Title>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
