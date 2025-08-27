538e2e580cce92d2b774b2262d33800aa1b33240
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-27 12:41:40 +0500

    Подключил react-router-dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import { Theme } from './Components/Theme/Theme.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { Theme } from "./Components/Theme/Theme.tsx";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Theme>
-      <App />
-    </Theme>
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Theme>
+        <App />
+      </Theme>
+    </BrowserRouter>
+  </StrictMode>
+);
