2530bf253d9747ba8fb4271451785c22699e1299
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-29 13:05:07 +0500

    Сдлелал нажатие на кнопку и вызов sudoku

--- client/src/Components/Hero/HeroGame/Sudoku/Sudoku.tsx
+++ client/src/Components/Hero/HeroGame/Sudoku/Sudoku.tsx
@@ -1,19 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import "./Sudoku.scss";
 
 export const Sudoku = () => {
-  const [board, setBoard] = useState([]);
-  const [boardChack, setBoardCheck] = useState([]);
+  const [board, setBoard] = useState<number[][]>([]);
+  const [boardCheck, setBoardCheck] = useState([]);
+  const [isCellCorrect, setIsCellCorrect] = useState([]);
 
   const fetchBoard = async () => {
     const response = await fetch("https://sudoku-api.vercel.app/api/dosuku");
     const data = await response.json();
-    setBoard(data)
-  }
-  
-  
+    setBoard(data.newboard.grids[0].value);
+  };
+
+  useEffect(() => {
+    fetchBoard();
+  }, []);
+
+  const handleChange = (rowIndex, colIndex, value: string) => {
+    if (/^[1-9]?$/.test(value)) {
+      const newBoard = board.map((row, rIdx) =>
+        row.map((cell, cIdx) =>
+          rIdx === rowIndex && cIdx === colIndex
+            ? value
+              ? parseInt(value)
+              : 0
+            : cell
+        )
+      );
+      const isCorrect = parseInt(value) === boardCheck
+    }
+  };
+
   return (
     <>
-      <h1>Sudoku</h1>
+      <div className="sudoku">
+        <div className="sudoku-board">
+          {board.map((row, rowIndex) => (
+            <div className="sudoku-row" key={rowIndex}>
+              {row.map((cell, coIndex) => {
+                const isInputIncorrect =
+                  isCellCorrect[rowIndex][coIndex] === false && cell !== 0;
+                return (
+                  <input
+                    type="text"
+                    key={coIndex}
+                    value={cell !== 0 ? cell : ""}
+                    onTouchEndCapture={(e) =>
+                      handleChange(rowIndex, coIndex, e.target.value)
+                    }
+                    readOnly={isCellCorrect[rowIndex][coIndex]}
+                    className={`sudoku-cell ${cell !== 0 ? "prewritten" : ""} ${
+                      isInputIncorrect ? "incorrect" : ""
+                    }`}
+                  />
+                );
+              })}
+            </div>
+          ))}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
