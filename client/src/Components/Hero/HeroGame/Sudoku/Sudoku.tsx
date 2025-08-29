b98b75fd7930516cc5e55bd7d9b32deeaf418882
Emil Gaynulin <emil.gaynulin99@bk.ru>
2025-08-29 12:30:20 +0500

    Функция fetch

--- client/src/Components/Hero/HeroGame/Sudoku/Sudoku.tsx
+++ client/src/Components/Hero/HeroGame/Sudoku/Sudoku.tsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 export const Sudoku = () => {
+  const [board, setBoard] = useState([]);
+  const [boardChack, setBoardCheck] = useState([]);
+
+  const fetchBoard = async () => {
+    const response = await fetch("https://sudoku-api.vercel.app/api/dosuku");
+    const data = await response.json();
+    setBoard(data)
+  }
+  
+  
   return (
     <>
       <h1>Sudoku</h1>
