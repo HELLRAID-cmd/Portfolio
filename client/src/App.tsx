4b437a494c96b4eb208697e301279bd1938a55b6
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-09-05 10:43:01 +0600

    Изменил название

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import "./styles/Reset.scss";
 import "./styles/container.scss";
-import "./styles/index.scss";
+import "./styles/Theme.scss";
 import { Header } from "./Components/Header/Header";
 import { Layout } from "antd";
 import { Hero } from "./Components/Hero/Hero";
