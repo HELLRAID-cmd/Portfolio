f2c57043163818b5d0a8633a370e878aecb64389
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-09-07 22:11:39 +0600

    Удалил winnerState

--- client/src/Components/Hero/HeroGame/X-O/GameInfo.tsx
+++ client/src/Components/Hero/HeroGame/X-O/GameInfo.tsx
@@ -1,16 +1,11 @@
-import { Button } from "antd";
-
 type GameInfoProps = {
   status: React.ReactNode;
-  winnerState: boolean;
-  onReset: () => void; 
 }
 
-const GameInfo = ({status, onReset, winnerState}: GameInfoProps) => {
+const GameInfo = ({status}: GameInfoProps) => {
   return (
     <div className="game-info">
       {status}
-      <Button onClick={onReset} className={`game-info__button button ${winnerState ? "game-info__button--winner" : ""}` }aria-label="reset game">Reset game</Button>
     </div>
   )
 }
