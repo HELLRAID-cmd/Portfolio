814207cff5da5dfa3014f6da40f8a73e0cd1cf5d
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-09-10 18:24:43 +0600

    Продублировал карточку и сделал обертку

--- client/src/Components/Project/Project.tsx
+++ client/src/Components/Project/Project.tsx
@@ -13,70 +13,136 @@ export const Project = () => {
                 My Portfolio
               </Typography.Title>
 
-              <Card className="project__card">
-                <picture>
-                  <source
-                    media="(max-width: 1000px)"
-                    src="/img/project/project.jpg"
-                    width={200}
-                  />
-                  <img
-                    className="project__card-img"
-                    src="./public/img/project/project.jpg"
-                    width={350}
-                    alt="img"
-                  />
-                </picture>
-                <div className="project__card-inner">
-                  <a
-                    className="project__card-inner-title link-title"
-                    href="https://github.com/HELLRAID-cmd/Briaton---finalWork"
-                    target="_blank"
-                  >
-                    Briaton Final Work
-                  </a>
-                  <div className="project__card-text">
-                    <p className="project__card-text-desc">
-                      The final assignment for the Skillbox courses includes
-                      an&nbsp;online store of&nbsp;various types
-                      of&nbsp;chandeliers. Rewritten in&nbsp;TS + React.
-                    </p>
-                    <p className="project__card-text-technologies">
-                      Technologies: TS, React, Vite, Scss, Just Validate.
-                    </p>
-                    <div className="project__card-technologies">
-                      <div className="project__card-technologies-img">
-                        <img
-                          src="./public/img/project/icons/React.png"
-                          width={35}
-                          alt="img"
-                        />
-                      </div>
-                      <div className="project__card-technologies-img">
-                        <img
-                          src="./public/img/project/icons/TypeScript.png"
-                          width={35}
-                          alt="img"
-                        />
-                      </div>
-                      <div className="project__card-technologies-img">
-                        <img
-                          src="./public/img/project/icons/Sass.png"
-                          width={35}
-                          alt="img"
-                        />
-                      </div>
-                      <div className="project__card-technologies-img">
-                        <img
-                          src="./public/img/project/icons/Vite.js.png"
-                          width={35}
-                          alt="img"
-                        />
+              <div className="project__cards">
+                <Card className="project__card">
+                  <picture>
+                    <source
+                      media="(max-width: 1000px)"
+                      src="/img/project/project.jpg"
+                      width={200}
+                    />
+                    <img
+                      className="project__card-img"
+                      src="./public/img/project/project.jpg"
+                      width={350}
+                      alt="img"
+                    />
+                  </picture>
+                  <div className="project__card-inner">
+                    <a
+                      className="project__card-inner-title link-title"
+                      href="https://github.com/HELLRAID-cmd/Briaton---finalWork"
+                      target="_blank"
+                    >
+                      Briaton Final Work
+                    </a>
+                    <div className="project__card-text">
+                      <p className="project__card-text-desc">
+                        The final assignment for the Skillbox courses includes
+                        an&nbsp;online store of&nbsp;various types
+                        of&nbsp;chandeliers. Rewritten in&nbsp;TS + React.
+                      </p>
+                      <p className="project__card-text-technologies">
+                        Technologies: TS, React, Vite, Scss, Just Validate.
+                      </p>
+                      <div className="project__card-technologies">
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/React.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/TypeScript.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/Sass.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/Vite.js.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </Card>
+                </Card>
+                <Card className="project__card">
+                  <picture>
+                    <source
+                      media="(max-width: 1000px)"
+                      src="/img/project/project.jpg"
+                      width={200}
+                    />
+                    <img
+                      className="project__card-img"
+                      src="./public/img/project/project.jpg"
+                      width={350}
+                      alt="img"
+                    />
+                  </picture>
+                  <div className="project__card-inner">
+                    <a
+                      className="project__card-inner-title link-title"
+                      href="https://github.com/HELLRAID-cmd/Briaton---finalWork"
+                      target="_blank"
+                    >
+                      Briaton Final Work
+                    </a>
+                    <div className="project__card-text">
+                      <p className="project__card-text-desc">
+                        The final assignment for the Skillbox courses includes
+                        an&nbsp;online store of&nbsp;various types
+                        of&nbsp;chandeliers. Rewritten in&nbsp;TS + React.
+                      </p>
+                      <p className="project__card-text-technologies">
+                        Technologies: TS, React, Vite, Scss, Just Validate.
+                      </p>
+                      <div className="project__card-technologies">
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/React.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/TypeScript.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/Sass.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/Vite.js.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Card>
+              </div>
             </div>
           </div>
         </div>
