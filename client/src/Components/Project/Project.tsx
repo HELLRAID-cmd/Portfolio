f68cb9f6c466a2cec95b55b8809c89edaa5bb9f2
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-09-10 22:51:06 +0600

    Подключил картинки и исправил порядок технологий

--- client/src/Components/Project/Project.tsx
+++ client/src/Components/Project/Project.tsx
@@ -43,7 +43,7 @@ export const Project = () => {
                         of&nbsp;chandeliers. Rewritten in&nbsp;TS + React.
                       </p>
                       <p className="project__card-text-technologies">
-                        Technologies: TS, React, Vite, Scss, Just Validate.
+                        Technologies: React, TS, Scss, Vite, Just Validate.
                       </p>
                       <div className="project__card-technologies">
                         <div className="project__card-technologies-img">
@@ -70,6 +70,13 @@ export const Project = () => {
                         <div className="project__card-technologies-img">
                           <img
                             src="./public/img/project/icons/Vite.js.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/JustValidate.png"
                             width={35}
                             alt="img"
                           />
@@ -98,16 +105,14 @@ export const Project = () => {
                       href="https://github.com/HELLRAID-cmd/Briaton---finalWork"
                       target="_blank"
                     >
-                      Briaton Final Work
+                      Crypto Project
                     </a>
                     <div className="project__card-text">
                       <p className="project__card-text-desc">
-                        The final assignment for the Skillbox courses includes
-                        an&nbsp;online store of&nbsp;various types
-                        of&nbsp;chandeliers. Rewritten in&nbsp;TS + React.
+                        A&nbsp;small app for practicing working with state and calculations. The user can &laquo;buy&raquo; cryptocurrency at&nbsp;any price and quantity, and then track how much he&nbsp;earned or&nbsp;lost.
                       </p>
                       <p className="project__card-text-technologies">
-                        Technologies: TS, React, Vite, Scss, Just Validate.
+                        Technologies: React, TS, Scss, Vite, Antd.
                       </p>
                       <div className="project__card-technologies">
                         <div className="project__card-technologies-img">
@@ -134,6 +139,13 @@ export const Project = () => {
                         <div className="project__card-technologies-img">
                           <img
                             src="./public/img/project/icons/Vite.js.png"
+                            width={35}
+                            alt="img"
+                          />
+                        </div>
+                        <div className="project__card-technologies-img">
+                          <img
+                            src="./public/img/project/icons/AntD.png"
                             width={35}
                             alt="img"
                           />
