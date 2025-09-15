11dcf321967e8b813af204e0d73d8e30d331c17d
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-09-15 20:43:05 +0600

    Добавил еще ссылки

--- client/src/Components/Footer/Footer.tsx
+++ client/src/Components/Footer/Footer.tsx
@@ -10,12 +10,57 @@ export const Footer = () => {
           <div className="footer__social">
             <ul className="footer__social-list">
               <li className="footer__social-item">
-                <a className="footer__social-link" href="https://github.com/HELLRAID-cmd/EmilGainulin" target="_blank">GitHub</a>
+                <a
+                  className="footer__social-link"
+                  href="https://github.com/HELLRAID-cmd/EmilGainulin"
+                  target="_blank"
+                >
+                  <svg
+                    className="footer__social-icon"
+                    width="30"
+                    height="30"
+                    aria-hidden="true"
+                  >
+                    <use href="/public/svg/sprite.svg#icon-github"></use>
+                  </svg>
+                </a>
+              </li>
+              <li className="footer__social-item">
+                <a
+                  className="footer__social-link"
+                  href="mailto:emilgaynulinjob@gmail.com"
+                  target="_blank"
+                >
+                  <svg
+                    className="footer__social-icon"
+                    width="30"
+                    height="30"
+                    aria-hidden="true"
+                  >
+                    <use href="/public/svg/sprite.svg#icon-mail"></use>
+                  </svg>
+                </a>
+              </li>
+              <li className="footer__social-item">
+                <a
+                  className="footer__social-link"
+                  href="#"
+                  target="_blank"
+                >
+                  <svg
+                    className="footer__social-icon"
+                    width="30"
+                    height="30"
+                    aria-hidden="true"
+                  >
+                    <use href="/public/svg/sprite.svg#icon-telegram"></use>
+                  </svg>
+                </a>
               </li>
             </ul>
           </div>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
