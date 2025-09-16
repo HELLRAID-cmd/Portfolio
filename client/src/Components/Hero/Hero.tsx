d6b8c1146b17f232cf8ef156ccfb9b81caeeb7fd
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-09-16 20:31:41 +0600

    Исправил разметку

--- client/src/Components/Hero/Hero.tsx
+++ client/src/Components/Hero/Hero.tsx
@@ -1,5 +1,5 @@
 import { ArrowDownOutlined } from "@ant-design/icons";
-import { Typography, Button } from "antd";
+import { Button } from "antd";
 import { Routes, Route, Link } from "react-router-dom";
 import { Sudoku } from "./HeroGame/Sudoku/Sudoku";
 import { HeroXO } from "./HeroGame/X-O/X-O";
@@ -15,48 +15,26 @@ export const Hero = () => {
         <div className="hero">
           <div className="container">
             <div className="hero__wrapper">
-              <Typography.Paragraph className="hero__text">
-                <Typography.Paragraph className="hero__text-wrapper">
-                  <Typography.Title
-                    level={1}
-                    className="hero__text-wrapper-title"
-                    data-text="Hello! My name is Emil Gaynulin"
-                  >
-                    Hello! My name is <br /> Emil Gaynulin
-                  </Typography.Title>
-                  <Typography.Title
-                    level={2}
-                    style={{ margin: 0 }}
-                    className="hero__text-wrapper-desc"
-                    data-text="This is what I can do."
-                  >
-                    This is what I can do.
-                  </Typography.Title>
-                </Typography.Paragraph>
+              <div className="hero__text">
+                <div className="hero__text-wrapper">
+                  <h1 className="hero__text-title" data-text="Hello! My name is Emil Gaynulin">Hello! My name is <br /> Emil Gaynulin</h1>
+                  <h2 className="hero__text-desc" data-text="This is what I can do.">This is what I can do.</h2>
+                </div>
                 <Button className="hero__text-button button">Check!</Button>
-              </Typography.Paragraph>
-
-              <Typography.Paragraph className="hero__game">
+              </div>
+              <div className="hero__game">
                 <div className="hero__game-links">
                   <Link to="/" onClick={() => setActive("sudoku")} className={`hero__game-link link ${active === "sudoku" ? "sudoku" : ""}`}>Sudoku</Link>
                   <Link to="/HeroXO" onClick={() => setActive("x-o")} className={`hero__game-link link ${active === "x-o" ? "x-o" : ""}`}>X-O</Link>
                 </div>
 
                 <div className="hero__game-inner">
-                <Routes>
-                  <Route path="/" element={<Sudoku />} />
-                  <Route path="/HeroXO" element={<HeroXO />} />
-                </Routes>
+                  <Routes>
+                    <Route path="/" element={<Sudoku />} />
+                    <Route path="/HeroXO" element={<HeroXO />} />
+                  </Routes>
                 </div>
-
-              </Typography.Paragraph>
-
-              <a href="#about" className="hero__wrapper-link" aria-label="Scroll down">
-                <ArrowDownOutlined
-                  className="hero__wrapper-link-arrow"
-                  height={40}
-                />
-              </a>
+              </div>
             </div>
           </div>
         </div>
