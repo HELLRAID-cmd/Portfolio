78c482a8382b657533c3ffa341d06be959534bf1
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-09-28 19:28:08 +0600

    Добавил прелоад страницы

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,8 +7,23 @@ import { Hero } from "./Components/Hero/Hero";
 import { About } from "./Components/About/About";
 import { Project } from "./Components/Project/Project";
 import { Footer } from "./Components/Footer/Footer";
+import { useEffect, useState } from "react";
+import { Loading } from "./Components/Loading/Loading";
 
-function App() {
+function App() { 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loading />
+  }
+  
   return (
     <Layout style={{ backgroundColor: "unset" }}>
       <Header />
