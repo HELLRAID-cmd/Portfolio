21d3e450a0c3460e865f005b03e469b672144570
HELLRAID-cmd <emil.gaynulin99@bk.ru>
2025-10-14 20:56:14 +0600

    функция для анимации

--- client/src/Components/About/About-Card.tsx
+++ client/src/Components/About/About-Card.tsx
@@ -1,11 +1,37 @@
+import { useEffect } from "react";
 import { skills } from "./About-skills";
 import { Card, Timeline } from "antd";
 
 export const Cards = () => {
+  useEffect(() => {
+    const scrollCard: NodeListOf<Element> =
+      document.querySelectorAll(".about__card-card");
+      const observer = new IntersectionObserver(
+      (entries: IntersectionObserverEntry[]) => {
+        entries.forEach((entry) => {
+          const target = entry.target as HTMLElement;
+          if (entry.isIntersecting) {
+            target.classList.add("visible");
+          }
+        });
+      },
+      {
+        threshold: .2, // 20% элемента
+      }
+    );
+
+    scrollCard.forEach((item) => observer.observe(item));
+
+    return () => {
+      scrollCard.forEach((item) => observer.unobserve(item));
+      observer.disconnect();
+    };
+  }, []);
+  
     return (
       <>
         {skills.map((group, i) => (
-          <Card key={i}>
+          <Card key={i} className="about__card-card">
             <Timeline
               items={group.map((item) => ({
                 key: item.id,
